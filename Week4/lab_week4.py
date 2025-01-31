e4173b89b3098ce22b7fe6c3ba0100daf79f3fa7
tirth1285 <rabadiyatirth8@gmail.com>
2025-01-31 08:47:36 -0500

    update lab4

--- Week4/lab_week4.py
+++ Week4/lab_week4.py
@@ -81,6 +81,15 @@ loot_options = ["Health Potion", "Poison Potion", "Secret Note", "Leather Boots"
 good_loot_options = ["Health Potion", "Leather Boots"]
 bad_loot_options = ["Poison Potion"]
 
+# Lab04 - Q1
+#Define the Monster Power
+monster_power = {
+    "Fire Magic": 2,
+    "freezing Time": 4,
+    "Super Hearing": 6,
+
+}
+
 # Define the number of stars awarded to the Player
 num_stars = 0
 
